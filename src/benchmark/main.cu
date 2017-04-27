0fa55ee7e00b1171c89fda6b09f767fa925b41db
suliaron <a.suli@astro.elte.hu>
2017-04-27 15:31:09 +0200

    GetTimeMs64() function was added

--- src/benchmark/main.cu
+++ src/benchmark/main.cu
@@ -5,6 +5,13 @@
 #include <iomanip>      // std::setw
 #include <fstream>
 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <sys/time.h>
+#include <ctime>
+#endif
+
 #include "tools.h"
 #include "type.h"
 #include "constants.h"
@@ -15,6 +22,10 @@
 
 using namespace std;
 using namespace redutil2;
+
+/* Remove if already defined */
+typedef long long int64;
+typedef unsigned long long uint64;
 
 typedef enum benchmark_output_name
 {
@@ -73,6 +84,50 @@ void body_body_grav_accel(const var3_t& ri, const var3_t& rj, var_t mj, var3_t& 
     ai.z += d2 * r_ij.z;
 }
 } /* namespace kernel */
+
+  /*
+  *  -- Returns the amount of milliseconds elapsed since the UNIX epoch. Works on both --
+  * Returns the amount of microseconds elapsed since the UNIX epoch. Works on both
+  * windows and linux.
+  */
+uint64 GetTimeMs64()
+{
+#ifdef _WIN32
+    /* Windows */
+    FILETIME ft;
+    LARGE_INTEGER li;
+
+    /* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
+    * to a LARGE_INTEGER structure. */
+    GetSystemTimeAsFileTime(&ft);
+    li.LowPart = ft.dwLowDateTime;
+    li.HighPart = ft.dwHighDateTime;
+
+    uint64 ret = li.QuadPart;
+    ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+                                 //ret /= 10000; /* From 100 nano seconds (10^-7) to 1 millisecond (10^-3) intervals */
+    ret /= 10; /* From 100 nano seconds (10^-7) to 1 microsecond (10^-6) intervals */
+
+    return ret;
+#else
+    /* Linux */
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+
+    uint64 ret = tv.tv_usec;
+    /* Convert from micro seconds (10^-6) to milliseconds (10^-3) */
+    //ret /= 1000;
+
+    /* Adds the seconds (10^0) after converting them to milliseconds (10^-3) */
+    //ret += (tv.tv_sec * 1000);
+    /* Adds the seconds (10^0) after converting them to microseconds (10^-6) */
+    ret += (tv.tv_sec * 1000000);
+
+    return ret;
+#endif
+}
+
 
 inline
 void body_body_grav_accel(const var3_t& ri, const var3_t& rj, var_t mj, var3_t& ai)
