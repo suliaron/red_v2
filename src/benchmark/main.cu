83e8e3585cefbf1248acc63ea8ba55d62a238d37
suliaron <a.suli@astro.elte.hu>
2017-04-28 15:35:54 +0200

    Implementing the kernels to compute gravitational acceleration of N-bodies. The VS2015 has problems to compile kernel invocations like <<< >>>. Fucking ...

--- src/benchmark/main.cu
+++ src/benchmark/main.cu
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <cfloat>
 
 #ifdef _WIN32
 #include <chrono>
@@ -11,6 +12,9 @@
 #include <sys/time.h>
 #include <ctime>
 #endif
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
 #include "tools.h"
 #include "type.h"
@@ -81,6 +85,39 @@ void body_body_grav_accel(const var3_t& ri, const var3_t& rj, var_t mj, var3_t& 
     ai.y += d2 * r_ij.y;
     ai.z += d2 * r_ij.z;
 }
+
+__global__
+void calc_grav_accel_naive(uint32_t n_obj, const var3_t* r, const nbp_t::param_t* p, var3_t* a)
+{
+    // i is the index of the SINK body
+    const uint32_t i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < n_obj)
+    {
+        a[i].x = a[i].y = a[i].z = 0.0;
+
+        var3_t r_ij = { 0.0, 0.0, 0.0 };
+        // j is the index of the SOURCE body
+        for (uint32_t j = 0; j < n_obj; j++)
+        {
+            if (i == j) continue;
+            r_ij.x = r[j].x - r[i].x;
+            r_ij.y = r[j].y - r[i].y;
+            r_ij.z = r[j].z - r[i].z;
+
+            // compute norm square of d vector [5 FLOPS] [3 read, 1 write]
+            var_t d2 = SQR(r_ij.x) + SQR(r_ij.y) + SQR(r_ij.z);
+            var_t d = sqrt(d2);
+            var_t s = K2 * p[j].mass / (d * d2);
+
+            // 6 FLOP
+            a[i].x += s * r_ij.x;
+            a[i].y += s * r_ij.y;
+            a[i].z += s * r_ij.z;
+        } // 36 FLOP
+    }
+}
+
 } /* namespace kernel */
 
   /*
@@ -603,6 +640,86 @@ void benchmark_CPU(uint32_t n_obj, uint2_t snk, uint2_t src, const var_t* h_y, c
 #endif
 
     print(PROC_UNIT_CPU, method_name[1], param_name[1], int_bound, n_obj, 1, Dt_CPU, Dt_GPU, o_result, true);
+}
+
+float2 gpu_calc_grav_accel_naive(uint32_t n_obj, unsigned int max_n_tpb, const var_t* d_y, const var_t* d_p, var_t* d_dy)
+{
+    static bool first_call = true;
+    static uint32_t n_last;
+    static unsigned int opt_n_tpb;
+
+    float2 result = { 0.0, FLT_MAX };
+
+    cudaEvent_t start, stop;
+    CUDA_SAFE_CALL(cudaEventCreate(&start));
+    CUDA_SAFE_CALL(cudaEventCreate(&stop));
+
+    if (first_call)
+    {
+        n_last = n_obj;
+        opt_n_tpb = 16;
+    }
+
+    // Number of space and velocity coordinates
+    const uint32_t nv = NDIM * n_obj;
+
+    // Create aliases
+    const var3_t* r = (var3_t*)d_y;
+    const nbp_t::param_t* p = (nbp_t::param_t*)d_p;
+    var3_t* a = (var3_t*)(d_dy + nv);
+
+    if (first_call || n_last != n_obj)
+    {
+        for (unsigned int n_tpb = 16; n_tpb <= max_n_tpb; n_tpb += 16)
+        {
+            dim3 grid((n_obj + n_tpb - 1) / n_tpb);
+            dim3 block(n_tpb);
+
+            CUDA_SAFE_CALL(cudaEventRecord(start, 0));
+
+            kernel::calc_grav_accel_naive<<< grid, block >>>(n_obj, r, p, a);
+            CUDA_CHECK_ERROR();
+
+            CUDA_SAFE_CALL(cudaEventRecord(stop, 0));
+            CUDA_SAFE_CALL(cudaEventSynchronize(stop));
+
+            float elapsed_time = 0.0f;
+            // Computes the elapsed time between two events
+            // (in milliseconds with a resolution of around 0.5 microseconds).
+            CUDA_SAFE_CALL(cudaEventElapsedTime(&elapsed_time, start, stop));
+
+            if (elapsed_time < result.y)
+            {
+                result.x = n_tpb;
+                result.y = elapsed_time;
+            }
+        }
+        opt_n_tpb = result.x;
+    }
+    else
+    {
+        dim3 grid((n_obj + opt_n_tpb - 1) / opt_n_tpb);
+        dim3 block(opt_n_tpb);
+
+        CUDA_SAFE_CALL(cudaEventRecord(start, 0));
+
+        kernel::calc_grav_accel_naive<<< grid, block >>>(n_obj, r, p, a);
+        CUDA_CHECK_ERROR();
+
+        CUDA_SAFE_CALL(cudaEventRecord(stop, 0));
+        CUDA_SAFE_CALL(cudaEventSynchronize(stop));
+
+        float elapsed_time = 0.0f;
+        // Computes the elapsed time between two events
+        // (in milliseconds with a resolution of around 0.5 microseconds).
+        CUDA_SAFE_CALL(cudaEventElapsedTime(&elapsed_time, start, stop));
+
+        result.x = opt_n_tpb;
+        result.y = elapsed_time;
+    }
+    first_call = false;
+
+    return result;
 }
 
 void benchmark(option& opt, ofstream& o_result)
